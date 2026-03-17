9483d738f75a9541ea2afbb9dbade49981cb99b8
Jhon Benites <jhon.benites@syftdata.com>
2026-03-17 17:58:53 -0500

    Refactor _document.tsx to improve script loading and add syft-rb2b.umd.js for enhanced tracking functionality

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,58 +1,67 @@
 import { ColorModeScript } from "@chakra-ui/react";
 import theme from "../theme";
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document(a) {
-    const includeSyft = a.dangerousAsPath !== "/forms2";
-    return (
-      <Html lang="en">
-        <Head>
-          <link
-            rel="apple-touch-icon"
-            sizes="76x76"
-            href="/static/favicons/apple-touch-icon.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/static/favicons/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/static/favicons/favicon-16x16.png"
-          />
-          <link rel="manifest" href="/static/favicons/manifest.json" />
-          {includeSyft && 
+  const includeSyft = a.dangerousAsPath !== "/forms2";
+  return (
+    <Html lang="en">
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="76x76"
+          href="/static/favicons/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/static/favicons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/static/favicons/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/static/favicons/manifest.json" />
+        {includeSyft && (
           <script
-          id="syft-loader"
-          type="text/javascript"
-          async
-          defer
-          src="http://localhost:4173/syft.umd.js"
-          data-api-key="test"
-          />}
-          <script
+            id="syft-loader"
+            type="text/javascript"
+            async
+            defer
+            src="http://localhost:4173/syft.umd.js"
+            data-api-key="test"
+          />
+        )}
+        <script
           dangerouslySetInnerHTML={{
             __html: `
     !function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.crossOrigin="anonymous",p.async=!0,p.src=s.api_host.replace(".i.posthog.com","-assets.i.posthog.com")+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="init capture register register_once register_for_session unregister unregister_for_session getFeatureFlag getFeatureFlagPayload isFeatureEnabled reloadFeatureFlags updateEarlyAccessFeatureEnrollment getEarlyAccessFeatures on onFeatureFlags onSessionId getSurveys getActiveMatchingSurveys renderSurvey canRenderSurvey getNextSurveyStep identify setPersonProperties group resetGroups setPersonPropertiesForFlags resetPersonPropertiesForFlags setGroupPropertiesForFlags resetGroupPropertiesForFlags reset get_distinct_id getGroups get_session_id get_session_replay_url alias set_config startSessionRecording stopSessionRecording sessionRecordingStarted captureException loadToolbar get_property getSessionProperty createPersonProfile opt_in_capturing opt_out_capturing has_opted_in_capturing has_opted_out_capturing clear_opt_in_out_capturing debug".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
-    posthog.init('phc_bcoaBa4uZdMp0CzXr7QhZRRpnNGgBRRZ1GStvuJVAgJ', {api_host: 'https://us.i.posthog.com', person_profiles: 'identified_only'})`
-          }} />
-          <script
-            type="text/javascript"
-            id="hs-script-loader"
-            async
-            defer
-            src="//js.hs-scripts.com/43753372.js"
-          />
-        </Head>
-        <body>
-          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
+    posthog.init('phc_bcoaBa4uZdMp0CzXr7QhZRRpnNGgBRRZ1GStvuJVAgJ', {api_host: 'https://us.i.posthog.com', person_profiles: 'identified_only'})`,
+          }}
+        />
+        <script
+          type="text/javascript"
+          id="hs-script-loader"
+          async
+          defer
+          src="//js.hs-scripts.com/43753372.js"
+        />
+        <script
+          type="text/javascript"
+          src="/static/syft-rb2b.umd.js"
+          async
+          defer
+          data-api-key="test"
+        />
+      </Head>
+      <body>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
